ab1065d1b1b34dc76979f48855ae344936d8a5f7
thejskhan <thejargalsaikhan@gmail.com>
2024-04-22 03:41:07 +0800

    vercel ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react(), solidJs(), tailwind()],
-  output: "server",
-  adapter: vercel()
 });
