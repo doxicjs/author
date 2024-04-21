d028dff91b34ca71f2d4083b7c7e3368d5ef400c
thejskhan <thejargalsaikhan@gmail.com>
2024-04-22 03:31:46 +0800

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), solidJs(), tailwind()]
+  integrations: [mdx(), react(), solidJs(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
