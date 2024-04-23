5efc65ffed77174378888ad600d3b0703c21d7ac
thejskhan <thejargalsaikhan@gmail.com>
2024-04-24 05:42:56 +0800

    fix dropdown colors

--- src/components/themeToggle.tsx
+++ src/components/themeToggle.tsx
@@ -65,7 +65,7 @@ export default function ThemeToggle() {
       <DropdownMenu.Portal>
         <DropdownMenu.Content
           align="end"
-          className="w-full border border-neutral-a6 rounded bg-gradient-to-b from-neutral-a1 backdrop-blur"
+          className="w-full border border-neutral-a6 rounded bg-neutral-3"
           sideOffset={8}
           style={{
             minWidth: '10rem',
@@ -74,7 +74,7 @@ export default function ThemeToggle() {
           <DropdownMenu.Label
             className={dxText(
               'heading-compact-01',
-              'px-3 py-2 border-neutral-a6 border-b'
+              'px-3 py-2 border-neutral-6 border-b bg-neutral-1'
             )}
           >
             Change Theme
@@ -87,7 +87,7 @@ export default function ThemeToggle() {
               className={dxText(
                 'body-compact-01',
                 cn(
-                  'p-3 flex gap-3 items-center hover:bg-neutral-a4 active:bg-neutral-a5',
+                  'p-3 flex gap-3 items-center hover:bg-neutral-4 active:bg-neutral-5',
                   key == theme && 'text-primary-11'
                 )
               )}
